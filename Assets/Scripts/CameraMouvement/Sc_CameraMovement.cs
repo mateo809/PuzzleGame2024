f26d9aae1eb2f7e95d3aa78973be3ce6a9e06262
MDKayzeur <mdricmaurice@gmail.com>
2024-12-05 13:43:11 +0100

    Camera : Camera rotation
    - Can rotate the camera around the map using buttons labeled "Go left" and "Go right"

--- Assets/Scripts/CameraMouvement/Sc_CameraMovement.cs
+++ Assets/Scripts/CameraMouvement/Sc_CameraMovement.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Sc_CameraMovement : MonoBehaviour
@@ -5,47 +6,74 @@ public class Sc_CameraMovement : MonoBehaviour
     [SerializeField] private float _speed;
     [SerializeField] private Transform _targetCenter;
     [SerializeField] private Sc_WayPoints _waypoints;
+    [SerializeField] private Vector3 offset;
 
-    private Transform _target;
+    private List<Quaternion> _target = new List<Quaternion>();
+    [SerializeField]  private bool _isTurningL;
+    [SerializeField]  private bool _isTurningR;
     private int _waypointindex = 0;
+
+    private void Update()
+    {
+        if (_isTurningL)
+        {
+            GoRight();
+        }
+        if (_isTurningR)
+        {
+            GoLeft();
+        }
+    }
 
     void Start()
     {
-        _target = _waypoints.points[0];
-    }
-
-    void Update()
-    {
-        Vector3 dir = _target.position - transform.position;
-        Vector3 dirCenter = _targetCenter.position - transform.position;
-        transform.Translate(dir.normalized * _speed * Time.deltaTime, Space.World);
-
-        if (dir != Vector3.zero)
-        {
-            Quaternion targetRotation = Quaternion.LookRotation(dirCenter);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 5f);
-        }
-
-        if (Vector3.Distance(transform.position, _target.position) <= 0.5f)
-        {
-            NextWaypoint();
-        }
+        _target.Add(new Quaternion(0, 0, 0, 1));
+        _target.Add(new Quaternion(0, 0.707106829f, 0, 0.707106829f));
+        _target.Add(new Quaternion(0, -1, 0, 0));
+        _target.Add(new Quaternion(0, -0.707106829f, 0, 0.707106829f));
     }
 
     public void NextWaypoint()
     {
-        if (_waypointindex >= _waypoints.points.Count - 1)
+        if (_isTurningR) return;
+        _waypointindex--;
+        if(_waypointindex < 0)
         {
-            _waypointindex = 0;
-            return;
+            _waypointindex = _target.Count - 1;
         }
-
-        _waypointindex++;
-        _target = _waypoints.points[_waypointindex];
+        _waypointindex %= _target.Count;
+        GoRight();
     }
 
     public void GoRight()
     {
+        _isTurningR = true;
+        Debug.Log(_target[_waypointindex]);
+        _targetCenter.transform.localRotation = Quaternion.Lerp(_targetCenter.transform.localRotation, _target[_waypointindex],Time.deltaTime * _speed);
 
+        if (Quaternion.Angle(_targetCenter.transform.localRotation,_target[_waypointindex]) < 0.5f)
+        {
+            _isTurningR = false;
+        }
+    }
+
+    public void LastWaypoint()
+    {
+        if (_isTurningR) return;
+        _waypointindex++;
+        _waypointindex %= _target.Count;
+        GoLeft();
+    }
+
+    public void GoLeft()
+    {
+        _isTurningR = true;
+        Debug.Log(_target[_waypointindex]);
+        _targetCenter.transform.localRotation = Quaternion.Lerp(_targetCenter.transform.localRotation, _target[_waypointindex], Time.deltaTime * _speed);
+
+        if (Quaternion.Angle(_targetCenter.transform.localRotation, _target[_waypointindex]) < 0.5f)
+        {
+            _isTurningR = false;
+        }
     }
 }
