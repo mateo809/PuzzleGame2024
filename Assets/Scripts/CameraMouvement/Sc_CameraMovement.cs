02b140f2a66cc79bd99930d069719aed6ee7df72
MDKayzeur <mdricmaurice@gmail.com>
2024-12-05 17:18:52 +0100

    CameraMovement
    Can go back to the last level of zoom that is further than the actual level of zoom.

--- Assets/Scripts/CameraMouvement/Sc_CameraMovement.cs
+++ Assets/Scripts/CameraMouvement/Sc_CameraMovement.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Sc_CameraMovement : MonoBehaviour
 {
@@ -15,6 +17,8 @@ public class Sc_CameraMovement : MonoBehaviour
     [SerializeField]  private bool _isTurningL;
     [SerializeField]  private bool _isTurningR;
     private int _waypointindex = 0;
+
+    private List<Vector3> _steps = new List<Vector3>(3){ Vector3.zero, Vector3.zero, Vector3.zero };
 
     enum GameObjectSize
     {
@@ -93,6 +97,30 @@ public class Sc_CameraMovement : MonoBehaviour
         }
     }
 
+    public void GoBack()
+    {
+        switch (_camera.orthographicSize)
+        {
+            case (int)GameObjectSize.Near:
+
+                _boomStick.transform.position = _steps[1];
+                _camera.orthographicSize = (int)GameObjectSize.Medium;
+                break;
+
+            case (int)GameObjectSize.Medium:
+
+                _boomStick.transform.position = _steps[2];
+                _camera.orthographicSize = (int)GameObjectSize.Far;
+                break;
+
+            case (int)GameObjectSize.Far:
+
+                _boomStick.transform.position = new Vector3(0,0,0);
+                _camera.orthographicSize = (int)GameObjectSize.Far;
+                break;
+        }
+    }
+
     private void DetectWayPoint()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -100,19 +128,49 @@ public class Sc_CameraMovement : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit))
         {
-            if (hit.collider.gameObject.CompareTag("WaypointMedium"))
+            Vector3 targetPos;
+            Debug.Log("ray hit");
+            switch (hit.collider.gameObject.tag)
             {
-                Vector3 targetPos = hit.transform.position;
-                _boomStick.transform.position = targetPos;
-                _camera.orthographicSize = 5;
-            }
+                case "WaypointSmall":
 
-            if (hit.collider.gameObject.CompareTag("WaypointSmall"))
-            {
-                Vector3 targetPos = hit.transform.position;
-                _boomStick.transform.position = targetPos;
-                _camera.orthographicSize = 2;
+                    targetPos = hit.transform.position;
+                    _boomStick.transform.position = targetPos;
+                    _camera.orthographicSize = (int)GameObjectSize.Near;
+                    Debug.Log("Waypoint small reached");
+                    _steps[0] = hit.collider.gameObject.transform.position;
+                    break; 
+
+                case "WaypointMedium":
+                    targetPos = hit.transform.position;
+                    _boomStick.transform.position = targetPos;
+                    _camera.orthographicSize = (int) GameObjectSize.Medium;
+                    _steps[1] = hit.collider.gameObject.transform.position;
+                    Debug.Log("Waypoint medium reached");
+                    break;
+
+                case "WaypointLarge":
+
+                    targetPos = hit.transform.position;
+                    _boomStick.transform.position = targetPos;
+                    _camera.orthographicSize = (int)GameObjectSize.Far;
+                    _steps[2] = hit.collider.gameObject.transform.position;
+                    Debug.Log("Waypoint large reached");
+                    break;
             }
+            //if (hit.collider.gameObject.CompareTag("WaypointMedium"))
+            //{
+            //    Vector3 targetPos = hit.transform.position;
+            //    _boomStick.transform.position = targetPos;
+            //    _camera.orthographicSize = (int)GameObjectSize.Medium;
+            //}
+
+            //if (hit.collider.gameObject.CompareTag("WaypointSmall"))
+            //{
+            //    Vector3 targetPos = hit.transform.position;
+            //    _boomStick.transform.position = targetPos;
+            //    _camera.orthographicSize = (int)GameObjectSize.Near;
+            //}
         }
     }
 }
