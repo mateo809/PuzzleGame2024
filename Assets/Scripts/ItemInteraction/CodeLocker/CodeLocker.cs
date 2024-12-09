3c2b9a0dca75ae2001fbf985e5d67ce6cbd3e8bb
mateo809 <150682933+mateo809@users.noreply.github.com>
2024-12-09 16:38:32 +0100

    Code Lock Finish

--- Assets/Scripts/ItemInteraction/CodeLocker/CodeLocker.cs
+++ Assets/Scripts/ItemInteraction/CodeLocker/CodeLocker.cs
@@ -1,67 +1,61 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEngine.Rendering;
+
 public class CodeLocker : MonoBehaviour
 {
+    [SerializeField] private List<Transform> _wheelList = new List<Transform>(); 
+    [SerializeField] private List<int> _correctCode = new List<int>(); 
+    private List<int> _currentCode = new List<int>(); 
 
-    [SerializeField]private List<Transform> _wheelList = new List<Transform>();
-    private Vector3 _wheelMidPos;
-    
-
-
-
+    private void Start()
+    {
+        for (int i = 0; i < _wheelList.Count; i++)
+        {
+            _currentCode.Add(0);
+        }
+    }
 
     private void DetectClickedWheel()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        //if (Physics.Raycast(ray, out hit))
-        Debug.DrawRay(ray.origin,ray.direction.normalized * 1000, Color.red, 10);
+        Debug.DrawRay(ray.origin, ray.direction.normalized * 1000, Color.red, 1);
 
         if (Physics.Raycast(ray, out hit, 1000))
         {
-            Debug.Log(hit.collider.gameObject.name);
             if (!hit.collider.gameObject.CompareTag("Wheel"))
-            {
                 return;
+            int wheelIndex = _wheelList.FindIndex(wheel => wheel == hit.transform); // find index hit 
+
+            if (wheelIndex != -1)
+            {
+                RotateWheel(hit.transform, wheelIndex);
             }
-            hit.transform.Rotate(hit.transform.forward, 36);
         }
+    }
 
-
-
-        //if (Physics.Raycast(ray, out hit, 1000))
-        //{
-        //    Debug.Log("HIT LOCK");  
-
-            //    Transform wheelTrans = _wheelList.Aggregate((currentSmallest, next) =>
-            //    (next.position - hit.point).magnitude < (currentSmallest.position - hit.point).magnitude ? next : currentSmallest);
-
-            //    wheelTrans.Rotate(wheelTrans.forward, 36);
-
-            //    //if (hit.point.y > _wheelMidPos.y)
-            //    //{
-            //    //    //tourner roue vers le bas
-            //    //    //Rotate by 36 degrees because 360 degrees / 10 faces
-            //    //    wheelTrans.Rotate(wheelTrans.forward, 36);
-            //    //}
-            //    //else
-            //    //{
-            //    //    //tourner roue vers le haut
-            //    //    wheelTrans.Rotate(wheelTrans.forward, -36);
-            //    //}
-
-            //}
+    private void RotateWheel(Transform wheel, int index)
+    {
+        wheel.Rotate(wheel.forward, -36);
+        _currentCode[index]++;
+        if (_currentCode[index] == 10) _currentCode[index] = 0;
+        CheckCode();
     }
 
     private void CheckCode()
     {
-
+        if (_currentCode.SequenceEqual(_correctCode)) //check if the two List are identical
+        {
+            Debug.Log("Open");
+        }
     }
 
-    private void OnMouseDown()
+    private void Update()
     {
-        DetectClickedWheel();
+        if (Input.GetMouseButtonDown(0))
+        {
+            DetectClickedWheel();
+        }
     }
 }
