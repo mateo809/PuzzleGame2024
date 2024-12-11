f9beb6f181cd9ebc4f4521a51df161d4dc38c4b5
0shiros <oli2014@gmx.fr>
2024-12-11 09:58:42 +0100

    Fixed bug Input Camera Movement

--- Assets/Scripts/Inputs/Sc_InputCameraMovment.cs
+++ Assets/Scripts/Inputs/Sc_InputCameraMovment.cs
@@ -2,22 +2,19 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Sc_InputCameraMovment : MonoBehaviour
-{   
-    private InputAction _moveCamera;
+{
+    [SerializeField] private InputActionReference _moveCamera;
     [SerializeField] private Sc_CameraMovement _scCameraMovement;
-
-    private void Start()
-    {      
-        _moveCamera = GetComponent<PlayerInput>().actions.FindAction("CameraMovment");
-    }   
+      
     public void MoveCamera()
     {
-        Vector2 direction = _moveCamera.ReadValue<Vector2>();
+        Vector2 direction = _moveCamera.action.ReadValue<Vector2>();        
+
         if (direction == Vector2.right)
         {
-            _scCameraMovement.NextWaypoint();            
+            _scCameraMovement.NextWaypoint();
         }
-        else if (direction == - Vector2.right)
+        else if (direction == -Vector2.right)
         {
             _scCameraMovement.LastWaypoint();
         }
