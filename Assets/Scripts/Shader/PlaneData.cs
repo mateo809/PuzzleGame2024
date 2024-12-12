5c252158b537b29ee2c54ac2ae40c9c66ece21d7
mateo809 <150682933+mateo809@users.noreply.github.com>
2024-12-12 12:08:05 +0100

    ElectricityManager and desactiveShadow

--- Assets/Scripts/Shader/PlaneData.cs
+++ Assets/Scripts/Shader/PlaneData.cs
@@ -1,15 +1,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//[ExecuteAlways]
 public class PlaneData : MonoBehaviour
 {
     public List<GameObject> Objects = new List<GameObject>();
     private List<Material> _materials = new List<Material>();
     public Material mat;
 
+    private int defaultLayer;
+    private int interactableLayer;
+
     void Start()
     {
+        defaultLayer = LayerMask.NameToLayer("Default");
+        interactableLayer = LayerMask.NameToLayer("InteractableObject");
+
         for (int i = 0; i < Objects.Count; i++)
         {
             Color c = Objects[i].GetComponent<MeshRenderer>().material.color;
@@ -28,14 +33,20 @@ public class PlaneData : MonoBehaviour
         {
             _materials[i].SetVector("_PlanePosition", transform.position);
             _materials[i].SetVector("_PlaneNormal", transform.up);
+            int lightIsOn = _materials[i].GetInt("_LightIsOn");
+            if (lightIsOn == 0 && Objects[i].layer != defaultLayer)
+            {
+                Objects[i].layer = defaultLayer;
+            }
         }
     }
-    [ContextMenu("Debug/DeactivateShadows")]
+
     public void DeactivateShadows()
     {
-        for (int i = 0; i < Objects.Count; i++) 
+        for (int i = 0; i < Objects.Count; i++)
         {
             _materials[i].SetInt("_LightIsOn", 1);
+            Objects[i].layer = interactableLayer;
         }
         Destroy(gameObject);
     }
