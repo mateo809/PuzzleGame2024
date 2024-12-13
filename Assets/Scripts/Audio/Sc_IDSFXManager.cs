a6ccea86bae664322e228c777f89f01d830eec87
0shiros <oli2014@gmx.fr>
2024-12-13 15:40:22 +0100

    Translate audio sounds

--- Assets/Scripts/Audio/Sc_IDSFXManager.cs
+++ Assets/Scripts/Audio/Sc_IDSFXManager.cs
@@ -15,9 +15,9 @@ public static class Sc_IDSFXManager
     public static int windID = 9;
     public static int coalID = 10;
     public static int stepStairsCellarID = 11;  
-    public static int ClosingDoorID = 12;    
-    public static int switchOffID = 13; 
-    public static int switchOnID = 14;  
+    public static int closingDoorID = 12;    
+    public static int switchLightOffID = 13; 
+    public static int switchLightOnID = 14;  
     public static int leverID = 15; 
     public static int circuitBreakerLeverID = 16;   
     public static int lightID = 17; 
@@ -29,11 +29,11 @@ public static class Sc_IDSFXManager
     public static int pickUpPaperID = 23;
     public static int pickUpItemID = 24;
     public static int padlockCylinderID = 25;
-    public static int pickUpLadder = 26;
+    public static int pickUpLadderID = 26;
     public static int waterBucketID = 27;
     public static int tapValveID = 28;
     public static int carID = 29;
-    public static int dropLadderID = 30;
+    public static int ladderFittingID = 30;
     public static int circuitBreakerElectricityID = 31;
     public static int electricityBackID = 32;
     public static int actionValidatedID = 33;
