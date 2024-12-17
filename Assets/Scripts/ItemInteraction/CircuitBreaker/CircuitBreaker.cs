9b66e3072bb471605f6b4dc43694f2b2154cb8c8
andyDPY <a.dupouy@lahorde.school>
2024-12-17 14:15:35 +0100

    Implementation vfx in game

--- Assets/Scripts/ItemInteraction/CircuitBreaker/CircuitBreaker.cs
+++ Assets/Scripts/ItemInteraction/CircuitBreaker/CircuitBreaker.cs
@@ -6,6 +6,7 @@ public class CircuitBreaker : InteractableObject
     public bool powerIsRepare = false;
 
     public ElectricityManager electricityManager;
+    [SerializeField] private GameObject _electrickParticles;
 
     [SerializeField] private Animator _animator;
 
@@ -22,16 +23,19 @@ public class CircuitBreaker : InteractableObject
             // Button up
             Debug.Log("OFF");
             _animator.SetBool("Activate", false);
+            _electrickParticles.SetActive(false);
         }
         else
         {
             // Button down
             Debug.Log("ON");
             _animator.SetBool("Activate", true);
+            _electrickParticles.SetActive(true);
             if (powerIsRepare)
             {
                 Debug.Log("The electricity is back!");
-                electricityManager.OnElectricityRestored();  
+                electricityManager.OnElectricityRestored();
+                Destroy(_electrickParticles);
             }
         }
     }
